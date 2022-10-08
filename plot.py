3ffdd89dbde1659458035f93d2fdff9ea1bc2613
Pál Patrik Varga <VargaPalPatrik@student.tudelft.nl>
2022-10-08 16:11:09 +0200

    cleanup

--- plot.py
+++ plot.py
@@ -11,89 +11,6 @@ import pandas as pd
 import jenkspy
 
 
-def parse_dict_year():  
-    deviza = {}
-    with open('data-deviza.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        spamreader.__next__()
-        for row in spamreader:
-            row = row[0].split(',')
-
-            date = dt.datetime.strptime(row[0].strip('"'),'%Y-%m-%d').date()
-            year = date.year
-            currency = row[1].strip('"')
-            val = (float)(row[2].strip('"'))
-
-            date = int(date.strftime("%j"))
-
-            if currency in deviza:
-                if year in deviza[currency]:
-                    deviza[currency][year][date] = val
-                else:
-                    deviza[currency][year] = {date:val}
-            else:
-                deviza[currency] = {year:{date: val}}
-
-        return deviza
-
-def parse_dict():  
-    deviza = {}
-    with open('data-deviza.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        spamreader.__next__()
-        for row in spamreader:
-            row = row[0].split(',')
-
-            date = dt.datetime.strptime(row[0].strip('"'),'%Y-%m-%d').date()
-            currency = row[1].strip('"')
-            val = (float)(row[2].strip('"'))
-
-            if currency in deviza:
-                deviza[currency][date] = val
-            else:
-                deviza[currency] = {date: val}
-            
-
-    return deviza
-
-def graph_random_year():
-    deviza = parse_dict_year()
-
-    usd = deviza["USD"]
-
-    for year in usd.keys():
-        x = np.array(list(usd[year])).reshape((-1,1))
-        y = np.array(list(usd[year].values()))
-
-        model = LinearRegression().fit(x,y)
-
-        usd[year]["model"]=model
-
-    year1 =random.randint(2000,2020)
-
-    data1 = usd[year1].copy()
-
-    model1 = data1.pop("model", None)
-
-    days1 = data1.keys()
-
-    intercept1 = model1.intercept_
-    slope1 = model1.coef_
-    avg1 = np.average(list(data1.values()))
-
-    data1_no_trend = [data1[key]-intercept1-key*slope1+avg1 for key in list(data1.keys())]
-
-    plt.title(year1)
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d'))
-    plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
-    plt.yticks(np.arange(0, 310, 20))
-    plt.scatter(days1,data1.values(), s=1, color='red')
-    plt.scatter(data1.keys(),data1_no_trend, s=1, color='black')
-    plt.gcf().autofmt_xdate()
-    plt.axline((0,intercept1), slope = slope1, color='red')
-
-    plt.show()
-
 def remove_trends(plot):
     df = parse_csv()
     ts = df['value']
@@ -133,13 +50,14 @@ def remove_trends(plot):
         for i in breaks_rpt:
             plt.axvline(i, color='red',linestyle='dashed')
         plt.grid()
-        plt.legend()
 
         plt.subplot(1,2,2)
         plt.scatter(df.date.values, notrend, s=1)
         plt.show()
 
-    return notrend
+    data = {'date': df.date.values, 'value': notrend}
+
+    return pd.DataFrame(data)
 
 def parse_csv():
     df = pd.read_csv('data-deviza.csv')
@@ -159,26 +77,5 @@ def pelt(ts):
     breakpoint_model.fit(y0)
     return [0] + breakpoint_model.predict(pen=10)
 
-
-def jenks(ts, n_breaks, y):
-    breaks = jenkspy.jenks_breaks(y, n_classes=n_breaks)
-    breaks_jkp = []
-    for v in breaks:
-        idx = ts.index[ts == v][0]
-        breaks_jkp.append(idx)
-    
-
-    plt.scatter(ts.index, ts, s=1, label='data')
-    plt.title('USD')
-    print_legend = True
-    for i in breaks_jkp:
-        if print_legend:
-            plt.axvline(i, color='red',linestyle='dashed', label='breaks')
-            print_legend = False
-        else:
-            plt.axvline(i, color='red',linestyle='dashed')
-    plt.grid()
-    plt.legend()
-    plt.show()
-
-notrend = remove_trends(True)
+notrend = remove_trends(False)
+print(notrend)
