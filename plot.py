cbd09ba915d0a2fadd022d06f4055e3a5399c994
Pál Patrik Varga <VargaPalPatrik@student.tudelft.nl>
2022-10-08 21:29:16 +0200

    suptitle

--- plot.py
+++ plot.py
@@ -11,8 +11,7 @@ import pandas as pd
 import jenkspy
 
 
-def remove_trends(plot):
-    df = parse_csv()
+def remove_trends(df, do_plot):
     ts = df['value']
 
     breaks = pelt(ts)
@@ -39,12 +38,12 @@ def remove_trends(plot):
 
         notrend = np.concatenate((notrend, (y-p).reshape(-1)))
 
-        if(plot):
+        if do_plot:
             plt.plot(d,p,c='grey')
             plt.scatter(d,y,s=1)
             breaks_rpt.append(ts.index[h-1])
 
-    if(plot):
+    if do_plot:
         breaks_rpt = pd.to_datetime(breaks_rpt)
 
         for i in breaks_rpt:
@@ -66,7 +65,7 @@ def parse_csv():
     df = df[df["iso_4217"] == currency ]
     df = df[df['date'] > dt.datetime(2000,1,1)]
     df.set_index(df['date'], inplace = True)
-    return df
+    return df, currency
 
 def pelt(ts):
     y0 = np.array(ts.tolist())
@@ -75,7 +74,7 @@ def pelt(ts):
     breakpoint_model.fit(y0)
     return [0] + breakpoint_model.predict(pen=10)
 
-def plot_nth(df, div = 10, i = 0, j = 0):
+def plot_nth(df, title, div = 10, i = 0, j = 0):
     split_large = np.array_split(df, div)
     split = split_large[i].value.values
     nth = df.iloc[j::div, :].value.values
@@ -94,7 +93,10 @@ def plot_nth(df, div = 10, i = 0, j = 0):
     hist2.hist(nth, density=True, bins =10)
     hist2.title.set_text("\u03C3 = " + str(np.std(nth)))
     
+    fig.suptitle(title)
     plt.show()
     
-notrend = remove_trends(False)
-plot_nth(notrend)
+
+df, title = parse_csv()
+notrend = remove_trends(df, False)
+plot_nth(notrend, title)
